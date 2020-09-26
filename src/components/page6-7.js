4a695c8a592dc0765bba6a0086b05a6c3afc7033
RyanQian223 <69494389+RyanQian223@users.noreply.github.com>
2020-09-26 19:14:10 +0800

    Update page6-7.js

--- src/components/page6-7.js
+++ src/components/page6-7.js
@@ -7,7 +7,19 @@ const Page = ({ match }) => {
 
   return (
     <div>
-      <h1>Page {id}</h1>
+      <h1>EVENTS</h1>
+      <h1 ></h1>
+      <h1>2019大事记</h1>
+      </div>
+        
+      <table border='4'CELLPADDING="2"  CELLSPACING="2" WIDTH="100%">
+          <tr>
+           <th>2019年2月</th>
+          </tr>
+          <tr>
+            <th>第二届领事会第六次会议，批 <br /> 准2019年工作计划即预算 </th>
+            
+          </tr>
     </div>
   );
 };
