40f123511bfd7b44230d0064f5668eee983fa55c
RyanQian223 <69494389+RyanQian223@users.noreply.github.com>
2020-10-07 13:26:17 +0800

    Update 小龄融合p13.js

--- src/components/小龄融合p13.js
+++ src/components/小龄融合p13.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import "../../src/styleesheet/base.css";
+import "../assets/styleesheet/base.css";
 function App() {
   return (
     <div>
